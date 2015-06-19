ef2c02ce97037e20f12cbdb507b1ba3f81e08495
Gaetan Boedec <gboedec@orange.fr>
2015-06-19 09:42:09 +0200

    Try new param

--- publish/public/laralytics.js
+++ publish/public/laralytics.js
@@ -1,42 +1,51 @@
 'use strict';
 
+(function() {
+  var param = {
+
+  };
+}();
+
 var laralytics = {
-  init: function(_api, _version) {
+
+  init: function() {
 
     // Object track with : screen size, mouse position, browser, all click event
     var payLoad = {};
 
     // Options of laralytics when DOM is loaded
     var options = {
-      API : _api || '/laralytics',
-      Version : _version || null,
+      API : '/laralytics',
+      Version : null,
     };
 
     payLoad.Version = options.Version;
 
     laralytics.sendPayload(payLoad, options);
+
   },
 
   sendPayload: function(_payLoad, _options) {
 
     var request = new XMLHttpRequest();
     request.open('POST', ''+ _options.API +'', true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    request.setRequestHeader('X-XSRF-TOKEN', decodeURIComponent(laralytics.getCookie('XSRF-TOKEN')));
-    request.send(_payLoad);
+    request.setRequestHeader('Content-Type', 
+      'application/x-www-form-urlencoded; charset=UTF-8');
+    request.setRequestHeader('X-XSRF-TOKEN', 
+      decodeURIComponent(laralytics.getCookie('XSRF-TOKEN')));
+    request.send(JSON.stringify(_payLoad));
   },
 
   getCookie: function(cname) {
-    var name = cname + "=";
+    var name = cname + '=';
     var ca = document.cookie.split(';');
     for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)===' ') c = c.substring(1);
-        if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+      var c = ca[i];
+      while (c.charAt(0)===' ') c = c.substring(1);
+      if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
     }
-    return "";
+    return '';
   },
 };
 
 laralytics.init();
-//# sourceMappingURL=laralytics.js.map
