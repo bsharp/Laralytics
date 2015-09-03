fe9479255a3634d4b56bd80c0bddede03a1f4a5b
nicolasbeauvais <nicolasbeauvais1@gmail.com>
2015-09-03 19:00:45 +0200

    Move controller to a separate class and don't use closure in route file to avoid breaking route:cache

---
+++ src/LaralyticsController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Bsharp\Laralytics;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+/**
+ * Class LaralyticsController
+ * @package Bsharp\Laralytics
+ */
+class LaralyticsController extends Controller
+{
+    /**
+     * @param Laralytics $laralytics
+     * @param Request    $request
+     *
+     * @return $this|\Illuminate\Http\JsonResponse
+     */
+    public function payload(Laralytics $laralytics, Request $request) {
+
+        $trackerCookie = null;
+
+        // Define payload
+        $payload = $request->only('info', 'click', 'custom');
+
+        // Check for tracking cookie if we have info in the payload
+        if (!empty($payload['info'])) {
+            $trackerCookie = $laralytics->checkGlobalCookie($request);
+        }
+
+        // Check for page cookie if we have info in the payload
+        if (!empty($payload['info'])) {
+
+            $hasPageCookie = $laralytics->checkPageCookie($request);
+
+            // Page cookie with request uuid
+            if ($hasPageCookie) {
+                // Insert payload
+                $laralytics->payload($request, $payload, !is_null($trackerCookie));
+            }
+        }
+
+        if (!is_null($trackerCookie)) {
+            return response()->json()->withCookie($trackerCookie);
+        }
+
+        return response()->json();
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -1,31 +1,3 @@
 <?php
 
-Route::post('laralytics', function (Bsharp\Laralytics\Laralytics $laralytics, Illuminate\Http\Request $request) {
-
-    $trackerCookie = null;
-
-    // Define payload
-    $payload = $request->only('info', 'click', 'custom');
-
-    // Check for tracking cookie if we have info in the payload
-    if (!empty($payload['info'])) {
-        $trackerCookie = $laralytics->checkGlobalCookie($request);
-    }
-
-    // Check for page cookie if we have info in the payload
-    if (!empty($payload['info'])) {
-        $hasPageCookie = $laralytics->checkPageCookie($request);
-
-        // Page cookie with request uuid
-        if ($hasPageCookie) {
-            // Insert payload
-            $laralytics->payload($request, $payload, !is_null($trackerCookie));
-        }
-    }
-
-    if (!is_null($trackerCookie)) {
-        return response()->json()->withCookie($trackerCookie);
-    }
-
-    return response()->json();
-});
+Route::post('laralytics', '\Bsharp\Laralytics\LaralyticsController@payload');
